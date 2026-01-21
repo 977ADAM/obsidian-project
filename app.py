5acccc11ca2188b395609f22cf35d51129f2da0a
977ADAM <977team.95@gmail.com>
2026-01-21 18:13:32 +0300

    add main.py

--- app.py
+++ app.py
@@ -2407,17 +2407,17 @@ class GraphView(QGraphicsView):
                 node.label.setOpacity(self._lod_current)
             self._lod_timer.stop()
 
-def main():
-    install_global_exception_hooks()
-    app = QApplication([])
-    # Ensure QSettings uses stable org/app identifiers.
-    app.setOrganizationName(APP_NAME)
-    app.setApplicationName(APP_NAME)
-    win = NotesApp()
-    win.show()
-    log.info("Приложение запущено, SID=%s", SESSION_ID)
-    app.exec()
+# def main():
+#     install_global_exception_hooks()
+#     app = QApplication([])
+#     # Ensure QSettings uses stable org/app identifiers.
+#     app.setOrganizationName(APP_NAME)
+#     app.setApplicationName(APP_NAME)
+#     win = NotesApp()
+#     win.show()
+#     log.info("Приложение запущено, SID=%s", SESSION_ID)
+#     app.exec()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
