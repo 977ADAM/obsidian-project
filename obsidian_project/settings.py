4eee5aa94dc67078fb7781130a3d34fdc4225569
977ADAM <977team.95@gmail.com>
2026-01-21 18:04:07 +0300

    fix

--- obsidian_project/settings.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from pathlib import Path
-
-APP_NAME = "obsidian-project"
-LOG_DIR = Path.home() / f".{APP_NAME}" / "logs"
-LOG_PATH = LOG_DIR / f"{APP_NAME}.log"
